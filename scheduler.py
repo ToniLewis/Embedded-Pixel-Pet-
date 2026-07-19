bf15b3d5d3f77f9eea2a43371b2a4be8d32f92ac
Ta'Bless (Toni) Lewis <151300129+ToniLewis@users.noreply.github.com>
2026-07-19 13:01:56 -0500

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -3,23 +3,34 @@ from typing import Callable, List
 
 
 class ScheduledTask:
-    def __init__(self, func: Callable, interval_seconds: float):
+    def __init__(self, func: Callable[[], None], interval_seconds: float):
         self.func = func
-        self.interval_seconds = interval_seconds
-        self.next_run = time.time() + interval_seconds
+        self.interval = interval_seconds
+        self.next_run_time = time.time() + self.interval
 
 
 class Scheduler:
+    """
+    Very simple task scheduler.
+    You can register functions to be called every N seconds.
+    """
+
     def __init__(self):
         self.tasks: List[ScheduledTask] = []
 
-    def add_task(self, func: Callable, interval_seconds: float):
-        task = ScheduledTask(func, interval_seconds)
-        self.tasks.append(task)
+    def add_task(self, func: Callable[[], None], interval_seconds: float):
+        """
+        Register a function to be called every `interval_seconds`.
+        """
+        self.tasks.append(ScheduledTask(func, interval_seconds))
 
-    def run_pending(self):
+    def update(self):
+        """
+        Called every frame from main.py.
+        Runs any tasks whose scheduled time has arrived.
+        """
         now = time.time()
         for task in self.tasks:
-            if now >= task.next_run:
+            if now >= task.next_run_time:
                 task.func()
-                task.next_run = now + task.interval_seconds
+                task.next_run_time = now + task.interval
